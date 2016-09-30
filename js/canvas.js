685e734eab233cfc7f54f3602e82709aba9b2234
Young Jun Ko <youngjun.ko@epfl.ch>
2016-09-30 15:21:58 +0200

    test

--- js/canvas.js
+++ js/canvas.js
@@ -32,8 +32,8 @@ procButton.addEventListener('click', function(evt){
     var data = dataCanvas.toDataURL('image/png');
     console.log(data) ;
 
-    $.getJSON('http://hjkl-yjk21.rhcloud.com/_add_numbers', {
-    //$.getJSON('http://localhost:7111/_add_numbers', {
+    //$.getJSON('http://hjkl-yjk21.rhcloud.com/_add_numbers', {
+    $.getJSON('http://localhost:7111/_add_numbers', {
         img: data
     }, function(data) {
         $("#result").text(data.result);
